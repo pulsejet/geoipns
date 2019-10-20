b7c10bf0df3fe2a0e5536c9227b4674a154d20db
Varun Patil <radialapps@gmail.com>
2019-10-20 18:18:16 +0530

    Add script to get data

---
+++ getdata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Download
+wget "https://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip"
+wget "https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip"
+
+# Cleanup old
+rm -rf GeoLite2-City-CSV_*
+rm -rf GeoLite2-ASN-CSV_*
+
+# Unzip
+unzip GeoLite2-City-CSV.zip
+unzip GeoLite2-ASN-CSV.zip
+
+# Get files
+rm -rf GeoLite*.csv
+mv ./GeoLite2-City-CSV_*/*.csv .
+mv ./GeoLite2-ASN-CSV_*/*.csv .
+
+# Cleanup
+rm -rf GeoLite2-City-CSV_*
+rm -rf GeoLite2-ASN-CSV_*
+rm -rf GeoLite2*.zip
