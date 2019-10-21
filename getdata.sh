eb0c3c34619da023df1b4cf778b7bb3b1c3bf015
Varun Patil <radialapps@gmail.com>
2019-10-21 06:33:43 +0530

    Move CSV to data folder

--- getdata.sh
+++ getdata.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Move to data directory
+mkdir -p ./data
+cd data
+
 # Download
 wget "https://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip"
 wget "https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip"
@@ -21,3 +25,5 @@ mv ./GeoLite2-ASN-CSV_*/*.csv .
 rm -rf GeoLite2-City-CSV_*
 rm -rf GeoLite2-ASN-CSV_*
 rm -rf GeoLite2*.zip
+
+cd ..
